ae811c411e0f5471e6de7629f8648f64fae5fc91
Egor <topolnyak012@gmail.com>
2015-08-31 18:08:33 +0600

    #style fixes

--- app/models/oven.rb
+++ app/models/oven.rb
@@ -7,4 +7,14 @@ class Oven
   field :volume_of_heater
   field :max_length
   field :diameter
+
+  field :type
+
+  has_mongoid_attached_file :image,
+                            :styles => {
+                                :original => ['640x480', :jpg]
+                            }
+
+
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
